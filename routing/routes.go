c4cb7f23f6cc0a8dae6acde8129ed8f4f18abc66
Silas <SilasTompkins@outlook.com>
2023-09-29 11:01:50 -0400

    lessened routing, from scratch it do be

--- routing/routes.go
+++ routing/routes.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"text/template"
 )
 
 type Post struct {
@@ -19,62 +18,15 @@ func check(e error) {
 }
 
 func StartHandlers() *http.ServeMux {
-    fmt.Println("hello")
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", GetIndex)
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	mux.HandleFunc("/home", GetHome)
-	mux.HandleFunc("/posts", GetPosts)
-	mux.HandleFunc("/pictures", GetPictures)
 	return mux
 }
 
 func GetIndex(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
 	http.ServeFile(w, r, "tmpl/index.html")
-}
-
-func GetHome(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /home request\n")
-	if r.Header.Get("Hx-Request") == "" {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	} else {
-		http.ServeFile(w, r, "tmpl/home.html")
-	}
-}
-
-func GetPosts(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /posts request\n")
-	tmpl, err := template.ParseFiles("tmpl/posts.html")
-	check(err)
-	Posts := []Post{
-		{
-			Title:   "title1",
-			Content: "content1",
-		},
-		{
-			Title:   "title2",
-			Content: "content2",
-		},
-		{
-			Title:   "title3",
-			Content: "content3",
-		},
-	}
-	if r.Header.Get("Hx-Request") == "" {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	} else {
-		tmpl.Execute(w, Posts)
-	}
-}
-
-func GetPictures(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /pictures request\n")
-	if r.Header.Get("Hx-Request") == "" {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	} else {
-		http.ServeFile(w, r, "tmpl/pictures.html")
-	}
 }
 
 func ServeStatic(w http.ResponseWriter, r *http.Request) {
